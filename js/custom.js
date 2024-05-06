498d62a00ba3d3b22d38adcce6e827eab9572455
muraakib <muraakib@gmail.com>
2024-05-07 00:39:40 +0600

    building-my-portfolio-website

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+var nav = document.getElementById("navBarMob")
+        function navCome() {
+            if (nav.style.left == `-105%`) {
+                nav.style.left = `0`
+            }
+            else {
+
+                nav.style.left = `-105%`
+            }
+        }
